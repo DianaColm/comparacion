7bfd9bd20ab949fdc812db3b7dfb32c4fb25dc40
DianaColm <dcolmenerog@yahoo.com.mx>
2017-10-24 09:32:21 -0500

    actualizando el trabajo

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var comparar = function (arr1, arr2){
-  if(arr1.length === arr2.length){
+  if(arr1.length !== arr2.length){
     return false;
   }else{
     for(var i =0; i <arr1.length; i++){
-      if(){
-
+      if(arr1[i] !== arr2[i]){
+     return false;
       }
     }
+    return true;
   }
 }
 array1 = [1,2,3,4,5];
-array2 = [1,2,3,4,5]
+array2 = [1,2,3,4,5];
 
 comparar(array1, array2);
