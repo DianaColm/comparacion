712af3301243ba4e5451107c32caa736c91660b2
DianaColm <dcolmenerog@yahoo.com.mx>
2017-10-24 09:11:47 -0500

    subiendo readme con imagen

---
+++ app.js
@@ -0,0 +1,15 @@
+var comparar = function (arr1, arr2){
+  if(arr1.length === arr2.length){
+    return false;
+  }else{
+    for(var i =0; i <arr1.length; i++){
+      if(){
+
+      }
+    }
+  }
+}
+array1 = [1,2,3,4,5];
+array2 = [1,2,3,4,5]
+
+comparar(array1, array2);
